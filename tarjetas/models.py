5ee16f4bd62ca587e2778fd77c0d39f104ae172c
Aldo <aldoandres045@gmail.com>
2023-11-25 18:51:20 -0300

    Unificacion

--- tarjetas/models.py
+++ tarjetas/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from autentificacion.models import CustomUser
 
 class Tarjeta(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     numero = models.CharField(max_length=16)
     titular = models.CharField(max_length=255)
     fecha_expiracion = models.DateField()
